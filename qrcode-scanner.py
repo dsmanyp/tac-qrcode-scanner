aa4e6c30d1053c01ffab945be920826fbe6818af
Darius Mann <dsmanyp@gmail.com>
2021-03-03 23:20:42 +0100

    add logging

--- qrcode-scanner.py
+++ qrcode-scanner.py
@@ -1,4 +1,7 @@
+import logging
+import sys
 import time
+from datetime import date
 from datetime import datetime
 
 import imutils
@@ -9,13 +12,32 @@ from pyzbar import pyzbar
 
 import config
 
+logging_filename = 'logs/{}.log'.format(datetime.utcnow().strftime("%Y-%m-%d"))
+
+logging.basicConfig(filename='logs/{}.log'.format(date.today().strftime("%Y_%m_%d")), level=logging.DEBUG,
+                    format='[%(asctime)s][%(levelname)s][%(filename)s][%(funcName)s][%(lineno)d]:%(message)s')
+
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+logger = logging.getLogger()
+
+
+def my_handler(type, value, tb):
+    logger.exception("Uncaught exception: {0}".format(str(value)))
+
+
+sys.excepthook = my_handler
+
 vs = VideoStream().start()
 time.sleep(2.0)
+
+logging.info('{}: STARTED SCANNING...'.format(datetime.now().isoformat()))
 
 while True:
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
     barcodes = pyzbar.decode(frame)
+
     for barcode in barcodes:
         data = {
             'scanner_uuid': config.SCANNER_UUID,
@@ -28,8 +50,19 @@ while True:
 
         if response.status_code == 201 or response.status_code == 200:
             beep(sound='coin')
-            print(barcode.data.decode("utf-8"))
+            logging.info(
+                '{}: SCAN SUCCESSFUL (UUID: {})'.format(datetime.now().isoformat(), barcode.data.decode("utf-8")))
+        elif 400 <= response.status_code <= 499:
+            beep(sound='error')
+            logging.error(
+                '{}: BAD REQUEST {}'.format(datetime.now().isoformat(), response.content))
+        elif response.status_code <= 500:
+            beep(sound='error')
+            logging.error(
+                '{}: SERVER ERROR {})'.format(datetime.now().isoformat(), response.content))
         else:
             beep(sound='error')
+            logging.error(
+                '{}: SOMETHING WENT WRONG {}'.format(datetime.now().isoformat(), response.content))
 
     time.sleep(3)
